774b4e5957e3dec2aea2daffebc02ec1b6449db4
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-10 23:51:27 -0700

    finish basic implementation of ticket routes

--- config.js
+++ config.js
@@ -2,3 +2,6 @@
 // console.log(process.env.DATABASE_URL);
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL;
+
+// exports.TEST_DATABASE_URL = process.env.TEST_DATABASE_URL||
+//                             global.env.TEST_DATABASE_URL;
--- server/router/route_tickets.js
+++ server/router/route_tickets.js
@@ -8,23 +8,132 @@ const express = require('express');
 const ticketRouter = express.Router();
 
 //Importing the ticket model
-const {Ticket}= require('../models/model_tickets');
+const {Ticket} = require('../models/model_tickets');
 
 /////////////////////////////////////////////////////////////////////////////////////
-///////////////                  GetTicket                 /////////////////////////
+///////////////                  Get Ticket                 /////////////////////////
 ///////////////////////////////////////////////////////////////////////////////////
 //getting all the tickets
 ticketRouter.get('/',(req,res)=>{
+  let message = '';
+
   Ticket
   .find()
   .exec()
   .then(tickets=>{
-    res.json(tickets.map(ticket=>ticket.apiRepr()));
+    return res.json(tickets.map(ticket=>ticket.apiRepr()));
   })
   .catch(err=>{
-    console.err('get all tickets error: ', err);
-    res.status(500).json({message:'Internal server error (get all tickets)'});
+    message = 'Internal Server Error (get all tickets)';
+    console.err('Get All Tickets Error: ', err);
+    return res.status(500).json({message});
   });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+///////////////               Create Ticket                /////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+//creating the tickets
+ticketRouter.post('/',(req,res)=>{
+  const ticketFields = {status:'Unassigned'};
+  const requiredFields = ['request','location','group'];
+  let message = '';
+  console.log('wassup           ',req.body);
+  requiredFields.map(field =>{ 
+    if(!(field in req.body)){
+      message = `Your body is missing the field: ${field}`;
+      console.error(message);
+      return res.status(400).json({message});
+    }
+
+    if(req.body[field].length<1){
+      message = `Your field (${field}) is empty`;
+      console.error(message);
+      return res.status(400).json({message});
+    }
+
+    ticketFields[field]=req.body[field];
+  });
+
+  Ticket
+  .create(ticketFields)
+  .then(ticket=>{
+    return res.status(201).json(ticket.apiRepr());
+  })
+  .catch(err=>{
+    message = 'Internal Server Error (Post Ticket)';
+    console.error(`Post Ticket Error: ${err}`);
+    return res.status(500).json({message});
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+///////////////               Update Ticket                /////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+//Updating Tickets
+ticketRouter.put('/:id',(req,res)=>{
+  let message = '';
+
+  if(!(req.params.id ===req.body.id)){
+    message = `$req params id: ${req.params.id} does not match req body id: ${req.body.id}`;
+    console.error(message);
+    return res.status(400).json({message});
+  }
+
+  const updateTicket = {};
+  const updateFields = ['request','location','status','group'];
+
+  updateFields.map(field =>{
+    if(field in req.body){
+      if(req.body[field].length<1){
+        message = `Your field (${field}) is empty`;
+        console.error(message);
+        return res.status(400).json({message});
+      }
+
+      updateTicket[field] = req.body[field];
+    }
+  });
+
+  Ticket
+  .findByIdAndUpdate(req.params.id, {$set: updateTicket}, {new:true})
+  .exec()
+  .then(ticket => {
+    return res.status(201).json(ticket.apiRepr());
+  })
+  .catch(err=>{
+    message = 'Internal server Error (update ticket)';
+    console.err(`Update Ticket Error: ${message}`);
+    return res.status(500).json({message});
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+///////////////               Delete Ticket                /////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+//Deleting Tickets
+ticketRouter.delete('/:id',(req,res)=>{
+  let message = '';
+
+  Ticket
+  .findByIdAndRemove(req.params.id)
+  .exec()
+  .then(ticket=>{
+    res.status(201).json(ticket.apiRepr());
+  })
+  .catch(err=>{
+    message = 'Internal Server Error (delete ticket)';
+    console.error(`Delete Ticket Error: ${message}`);
+    return res.status(500).json({message});
+  });
+});
+
+/////////////////////////////////////////////////////////////////////////////////////
+///////////////              Page Not Found                /////////////////////////
+///////////////////////////////////////////////////////////////////////////////////
+//Page Not Found
+ticketRouter.use('*',(req,res)=>{
+  res.status(404).json({message:'Page Not Found'});
 });
 
 /////////////////////////////////////////////////////////////////////////////////////
