f26da1bbb28289b9ba9217dbcee0dfaf624af038
Jamie Wang <sephona@gmail.com>
2017-07-11 15:49:17 -0400

    took out unnecessary variables

--- client/src/components/main-page.js
+++ client/src/components/main-page.js
@@ -1,6 +1,5 @@
 import React from 'react';
-import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
-import {submitTicket} from '../actions';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
 
 import Navbar from './navbar';
 import SignUp from './sign-up';
