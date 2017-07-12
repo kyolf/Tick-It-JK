c70210861af2d52dc43c7707aa2b5ee379100257
Kyle Szeto <kyl3sz3t0@gmail.com>
2017-07-11 21:58:41 -0700

    fix login linking page bug

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -37,15 +37,14 @@ export class Login extends React.Component{
   render(){
     return (
       <div className="login-container">
-         <form className="login-form">
+         <form className="login-form" onSubmit={e => this.logIn(e)}>
             <label htmlFor="username">Username</label>
             <input type="text" id="username" name="username" placeholder="Enter username" 
               minLength="1" required ref={username => this.username = username}/>
-            <label htmlFor="location">Password</label>
+            <label htmlFor="password">Password</label>
             <input type="text" id="password" name="password" placeholder="Enter password"
               minLength="6" required ref={password => this.password = password}/>
-            <input type="submit" id="login-submit" className="button" name="submit-login"
-              onSubmit={e => this.logIn(e)} />
+            <input type="submit" id="login-submit" className="button" name="submit-login"/>
           </form>
       </div>
     );
